fb8da65bc35cbe0b940486498788744a3ccdf3fd
Yann Lerjen <yann.lerjen@gmail.com>
2018-02-07 22:47:56 +0100

    complete join session form

--- src/app/home/join-session-form/join-session-form.component.ts
+++ src/app/home/join-session-form/join-session-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { GeneratorService } from '../../services/generator/generator.service';
+import { UserGeneratorResponse } from '../../models/UserGeneratorResponse';
 
 @Component({
   selector: 'app-join-session-form',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JoinSessionFormComponent implements OnInit {
 
-  constructor() { }
+  @Output()
+  public joinSessionEvent = new EventEmitter<string>();
+
+  public joinSessionForm: FormGroup;
+
+  constructor(
+    private _fb: FormBuilder,
+    private _genSrvc: GeneratorService
+  ) { }
 
   ngOnInit() {
+    this.createForm();
   }
 
+  createForm() {
+    this.joinSessionForm = this._fb.group({
+      text: ['', Validators.required],
+      username: ['', Validators.maxLength(12)]
+    });
+  }
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+    if (this.joinSessionForm.valid) {
+      this.joinSessionEvent.emit();
+    }
+  }
+
+  setRandomUsername() {
+    this._genSrvc.genUsername().
+      subscribe(
+        (resp: UserGeneratorResponse) => this.joinSessionForm.controls.username.setValue(resp.login.username),
+        (err) => alert('Can t generate a username')
+      );
+  }
 }
