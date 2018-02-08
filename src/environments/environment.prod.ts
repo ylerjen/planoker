4d7408cfc44faaf2196c31a07a9523d8fef5c5c9
Yann Lerjen <yann.lerjen@gmail.com>
2018-02-09 00:27:09 +0100

    firebase config also for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB5nZUAzoRW5Ch-pQbDDmeTL0CDL9GjUYs',
+    authDomain: 'planoker-5199b.firebaseapp.com',
+    databaseURL: 'https://planoker-5199b.firebaseio.com',
+    projectId: 'planoker-5199b',
+    storageBucket: '',
+    messagingSenderId: '244806065981'
+  }
 };
