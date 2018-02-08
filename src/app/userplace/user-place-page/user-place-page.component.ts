2d25a03a8e73ac57c552895e773a41a148855fb6
Yann Lerjen <yann.lerjen@gmail.com>
2018-02-09 00:25:26 +0100

    add user-place behaviors

--- src/app/userplace/user-place-page/user-place-page.component.ts
+++ src/app/userplace/user-place-page/user-place-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+
+import { VoteCommand } from '../../models/FirebaseCommand';
+import { FirebaseService } from '../../services/firebase/firebase.service';
 
 const conversionObj = {
   '?': '❓',
@@ -19,12 +24,51 @@ const conversionObj = {
   templateUrl: './user-place-page.component.html',
   styleUrls: ['./user-place-page.component.scss']
 })
-export class UserPlacePageComponent {
+export class UserPlacePageComponent implements OnInit {
 
   public selectOptions = conversionObj;
+ 
+  public isConfirmed: boolean;
+ 
+  public isPageReady: boolean;
+
   public selectedValue = conversionObj['?'];
+  private sessionId = '';
+  private username = '';
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _fireSrvc: FirebaseService
+  ) {}
+
+  ngOnInit() {
+    console.log('oninit');
+    this._route.params
+      .subscribe(
+        (routeData: Params) => {
+
+    console.log('oninit ok ', routeData);
+          this.sessionId = routeData.sid;
+          this.username = routeData.uid;
+          this.isPageReady = true;
+        },
+        (err) => console.error(err),
+        () => this.isPageReady = true
+    );
+  }
+
+  onClickConfirmation($event: Event) {
+    const voteCmd: VoteCommand = {
+      username: this.username,
+      sessionId: this.sessionId,
+      vote: this.selectedValue
+    };
+    this._fireSrvc.storeVote(voteCmd);
+    this.isConfirmed = true;
+  }
 
   selectionChanged(evt: Event) {
+    this.isConfirmed = false;
     const selectEl = evt.target as HTMLSelectElement;
     this.selectedValue = conversionObj[selectEl.value];
   }
