63f9fbc10e72a18b3ba714db1f3c4be17293f72b
Yann Lerjen <yann.lerjen@gmail.com>
2018-02-11 22:34:48 +0100

    use dashboard comp

--- src/app/router.config.ts
+++ src/app/router.config.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 
 import { HomePageComponent } from './home/home-page/home-page.component';
-import { SessionPageComponent } from './dashboard/session-page/session-page.component';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 import { UserPlacePageComponent } from './userplace/user-place-page/user-place-page.component';
 
 export const appRoutes: Routes = [
     { path: '', component: HomePageComponent },
-    { path: 'session/:sid', pathMatch: 'full', component: SessionPageComponent },
+    { path: 'session/:sid', pathMatch: 'full', component: DashboardComponent },
     { path: 'session/:sid/user/:uid', component: UserPlacePageComponent },
     { path: '**', redirectTo: '/' }
 ];
