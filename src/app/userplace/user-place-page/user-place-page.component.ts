5698a22fcc8cace0cf2f25778c45af07e720f71f
Lerjen Yann <ylerjen@vaudoise.ch>
2018-02-16 14:57:35 +0100

    use store

--- src/app/userplace/user-place-page/user-place-page.component.ts
+++ src/app/userplace/user-place-page/user-place-page.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
 
+import { IUserlistState } from '../../stores/reducers/userlist/userlist.reducer';
 import { VoteCommand } from '../../models/FirebaseCommand';
+import { User } from '../../models/User';
 import { FirebaseService } from '../../services/firebase/firebase.service';
 
 const conversionObj = {
@@ -20,56 +23,67 @@ const conversionObj = {
 };
 
 @Component({
-  selector: 'app-user-place-page',
-  templateUrl: './user-place-page.component.html',
-  styleUrls: ['./user-place-page.component.scss']
+    selector: 'app-user-place-page',
+    templateUrl: './user-place-page.component.html',
+    styleUrls: ['./user-place-page.component.scss']
 })
-export class UserPlacePageComponent implements OnInit {
+export class UserPlacePageComponent implements OnInit, OnDestroy {
+    public selectOptions = conversionObj;
 
-  public selectOptions = conversionObj;
+    public currUser: User;
 
-  public isFrozen: boolean;
+    public isPageReady: boolean;
 
-  public isPageReady: boolean;
+    public selectedValue = conversionObj['?'];
 
-  public selectedValue = conversionObj['?'];
+    private sessionId = '';
 
-  private sessionId = '';
-  private username = '';
+    private _userlistStoreSubs$;
 
-  constructor(
-    private _route: ActivatedRoute,
-    private _fireSrvc: FirebaseService
-  ) { }
+    constructor(
+        private _route: ActivatedRoute,
+        private _fireSrvc: FirebaseService,
+        private _store: Store<IUserlistState>
+    ) {}
 
-  ngOnInit() {
-    this._route.params
-      .subscribe(
-        (routeData: Params) => {
+    ngOnInit() {
+        this._userlistStoreSubs$ = this._store
+            .select('userlistState')
+            .subscribe(refreshValuesFromStore);
+        this._route.params.subscribe(
+            (routeData: Params) => {
+                console.log('oninit ok ', routeData);
+                this.sessionId = routeData.sid;
+                this.currUser.username = routeData.uid;
+                this.isPageReady = true;
+            },
+            err => console.error(err),
+            () => (this.isPageReady = true)
+        );
+    }
 
-          console.log('oninit ok ', routeData);
-          this.sessionId = routeData.sid;
-          this.username = routeData.uid;
-          this.isPageReady = true;
-        },
-        (err) => console.error(err),
-        () => this.isPageReady = true
-      );
-  }
+    refreshValuesFromStore(val: IUserlistState) {
+        const currUser = val.userList[this.currUser.username];
+        currUser.username = this.currUser.username;
+        this.currUser = currUser;
+    }
 
-  onClickFreeze($event: Event) {
-    this.isFrozen = !this.isFrozen;
-    const voteCmd: VoteCommand = {
-      username: this.username,
-      sessionId: this.sessionId,
-      vote: this.selectedValue,
-      isFrozen: this.isFrozen
-    };
-    this._fireSrvc.storeVote(voteCmd);
-  }
+    ngOnDestroy() {
+        this._userlistStoreSubs$.unsubscribe();
+    }
 
-  selectionChanged(evt: Event) {
-    const selectEl = evt.target as HTMLSelectElement;
-    this.selectedValue = conversionObj[selectEl.value];
-  }
+    onClickFreeze($event: Event) {
+        this.currUser.isFrozen = !this.currUser.isFrozen;
+        const voteCmd: VoteCommand = {
+          username: this.currUser.username,
+          sessionId: this.sessionId,
+          vote: this.selectedValue,
+          isFrozen: this.currUser.isFrozen};
+        this._fireSrvc.storeVote(voteCmd);
+    }
+
+    selectionChanged(evt: Event) {
+        const selectEl = evt.target as HTMLSelectElement;
+        this.selectedValue = conversionObj[selectEl.value];
+    }
 }
