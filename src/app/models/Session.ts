8f02eafe2ad817d7566cea46a5875b04ebedab96
Lerjen Yann <ylerjen@vaudoise.ch>
2018-02-16 18:27:01 +0100

    add models

---
+++ src/app/models/Session.ts
@@ -0,0 +1,11 @@
+export class Session {
+         public sessionId: string;
+         public username: string;
+         public isRevealed: boolean;
+
+         constructor(props: { [key: string]: any } = {}) {
+           this.sessionId = props.sessionId;
+           this.username = props.username;
+           this.isRevealed = props.isRevealed;
+         }
+       }
--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,5 +1,11 @@
 export class User {
-    vote: string;
-    isFrozen: boolean;
-    username: string;
+    public vote: string;
+    public isFrozen: boolean;
+    public username: string;
+
+    constructor(props: { [key: string]: any } = {}) {
+        this.isFrozen = props.isFrozen;
+        this.username = props.username;
+        this.vote = props.vote;
+    }
 }
