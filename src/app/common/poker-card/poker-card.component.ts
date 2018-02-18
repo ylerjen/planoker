3c3e454eebf2e3b1275a6870e9b7efb956ade0aa
Yann Lerjen <yann.lerjen@gmail.com>
2018-02-18 23:08:53 +0100

    minor changes, convention

--- src/app/common/poker-card/poker-card.component.ts
+++ src/app/common/poker-card/poker-card.component.ts
@@ -1,9 +1,9 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: "app-poker-card",
-  templateUrl: "./poker-card.component.html",
-  styleUrls: ["./poker-card.component.scss"]
+  selector: 'app-poker-card',
+  templateUrl: './poker-card.component.html',
+  styleUrls: ['./poker-card.component.scss']
 })
 export class PokerCardComponent {
   @Input() public value: string;
