2afc7691cd2c34bc0b9712372a0beafc7a098d02
Yann Lerjen <yann.lerjen@gmail.com>
2018-02-18 23:08:20 +0100

    add missing actions

--- src/app/actions/session.action.ts
+++ src/app/actions/session.action.ts
@@ -1,19 +1,47 @@
 import { Action } from '@ngrx/store';
 import { ISessionState } from '../stores/reducers/session/session.reducer';
 
-export interface InitSessionStartAction extends Action {
+export interface IInitSessionStartAction extends Action {
     payload: ISessionState;
+}
+export interface ISetSessionIdOrUsernameAction extends Action {
+    payload: string;
+}
+export interface ISetRevealedStatusAction extends Action {
+    payload: boolean;
 }
 
 export enum ESessionActions {
     InitSessionStart = 'InitSessionStart',
-    SetUsername = 'SET_USERNAME',
-    SetSessionId = 'SET_SESSION_ID',
+    SetUsername = 'SetUsername',
+    SetSessionId = 'SetSessionId',
+    SetRevealedStatus = 'SetRevealedStatus',
 }
 
-export function initSessionStore(payload: ISessionState): InitSessionStartAction {
+export function initSessionStore(payload: ISessionState): IInitSessionStartAction {
     return {
         type: ESessionActions.InitSessionStart,
         payload
+    };
+}
+
+export function setSessionId(sessionId: string) {
+    return {
+        type: ESessionActions.SetSessionId,
+        payload: sessionId
+    };
+}
+
+export function setUsername(username: string) {
+    return {
+        type: ESessionActions.SetUsername,
+        payload: username
+    };
+}
+
+export function setRevealStatus(isRevealed: boolean) {
+    return {
+        type: ESessionActions.SetRevealedStatus,
+        payload: isRevealed
     };
 }
--- src/app/stores/reducers/session/session.reducer.ts
+++ src/app/stores/reducers/session/session.reducer.ts
@@ -1,29 +1,44 @@
 import { Action } from '@ngrx/store';
 
-import { ESessionActions, InitSessionStartAction } from '../../../actions/session.action';
+import {
+    ESessionActions,
+    IInitSessionStartAction,
+    ISetSessionIdOrUsernameAction,
+    ISetRevealedStatusAction
+} from '../../../actions/session.action';
+
 
 export interface ISessionState {
     sessionId: string;
     username: string;
+    isRevealed?: boolean;
 }
 
-export const initialState: ISessionState = { sessionId: '', username: '' };
+export const initialState: ISessionState = {
+    sessionId: '',
+    username: '',
+    isRevealed: false
+};
 
 export function sessionReducer(state: ISessionState = initialState, action: Action) {
-           switch (action.type) {
-               case ESessionActions.InitSessionStart:
-                    const actionWithPayload = action as InitSessionStartAction;
-                   return Object.assign({}, actionWithPayload.payload);
+    switch (action.type) {
+        case ESessionActions.InitSessionStart:
+            const actionWithPayload = action as IInitSessionStartAction;
+            return Object.assign({}, actionWithPayload.payload);
 
-               case ESessionActions.SetSessionId:
-                   throw new Error('not implemented');
-                   // return state;
+        case ESessionActions.SetSessionId:
+            const actionWithIdPayload = action as ISetSessionIdOrUsernameAction;
+            return Object.assign({}, state, { sessionId: actionWithIdPayload.payload });
 
-                case ESessionActions.SetUsername:
-                    throw new Error('not implemented');
-                    // return state;
+        case ESessionActions.SetUsername:
+            const actionWithUsernamePayload = action as ISetSessionIdOrUsernameAction;
+            return Object.assign({}, state, { username: actionWithUsernamePayload.payload });
 
-               default:
-                   return state;
-           }
-       }
+        case ESessionActions.SetRevealedStatus:
+            const actionWithRevealStatusPayload = action as ISetRevealedStatusAction;
+            return Object.assign({}, state, { isRevealed: actionWithRevealStatusPayload.payload });
+
+        default:
+            return state;
+    }
+}
