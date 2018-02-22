c41f87afd57bef29d1b1a0bd87df3f05c62ab3ab
Yann Lerjen <yann.lerjen@gmail.com>
2018-02-22 20:36:07 +0100

    url rewriting

--- server.js
+++ server.js
@@ -7,13 +7,9 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-// rewrite all non-static URLs back to index.html
-app.configure(function() {
-    app.use('/', express.static(__dirname + '/'));
-});
 // history API generated paths rewriting
 app.get('*', function(request, response, next) {
-    response.sendfile(__dirname + '/index.html');
+    response.sendFile(__dirname + '/dist/index.html');
 });
 
 // Start the app by listening on the default Heroku port
